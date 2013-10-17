8a6f8367b17bfe2a22fdb868b1d4916b61c0140f
Geoffrey Jones <jonesg5@rpi.edu>
2013-10-17 11:18:02 -0400

    Added ability to save output

--- batchFirstLast.m
+++ batchFirstLast.m
@@ -13,12 +13,23 @@ workbookFile = fullfile(inputDir,'index.xlsx');
 %% Assign constants
 firstHrs = 2; % Number of hours after waking
 lastHrs = 3; % Number of hours before bed
-n1 = length(dirInfo); % Number of files to be analyzed
+nFiles = length(dirInfo); % Number of files to be analyzed
 
 %% Preallocate output variables
+out = dataset;
+out.reference = cell(nFiles,1);
+out.subject = cell(nFiles,1);
+out.protocol = cell(nFiles,1);
+out.stage = cell(nFiles,1);
+out.firstCS = cell(nFiles,1);
+out.lastCS = cell(nFiles,1);
+out.firstCLA = cell(nFiles,1);
+out.lastCLA = cell(nFiles,1);
+out.firstLux = cell(nFiles,1);
+out.lastLux = cell(nFiles,1);
 
 %% Analyze files
-for i1 = 1:n1
+for i1 = 1:nFiles
     % Load the file
     load(fullfile(inputDir,dirInfo(i1).name),'sourceData');
     fileName = regexprep(dirInfo(i1).name,'(.*)\.mat','$1');
@@ -26,21 +37,25 @@ for i1 = 1:n1
     fileParts = regexpi(fileName,...
         'ref(\d*)_(\d\d\d\d\d\d)-(\d\d\d\d\d\d)_sub(\d\d)_(\w*)_(\w*)',...
         'tokens');
-%     curReference{i1} = fileParts{1}{1};
-    curSubject{i1} = fileParts{1}{4};
-    curProtocol{i1} = fileParts{1}{5};
-    curStage{i1} = fileParts{1}{6};
+    out.reference{i1} = fileParts{1}{1};
+    out.subject{i1} = fileParts{1}{4};
+    out.protocol{i1} = fileParts{1}{5};
+    out.stage{i1} = fileParts{1}{6};
     % Find matching index entry
-    idx = strcmpi(curSubject{i1},subject);
+    idx = strcmpi(out.subject{i1},subject);
     %% Find first and last values
-    [firstCS{i1},lastCS{i1}] = firstLast(sourceData.Time,...
-        sourceData.CS,wakeTime(idx),bedTime(idx),2,3);
-    [firstCLA{i1},lastCLA{i1}] = firstLast(sourceData.Time,...
-        log(sourceData.CLA),wakeTime(idx),bedTime(idx),2,3);
-    [firstLux{i1},lastLux{i1}] = firstLast(sourceData.Time,...
-        log(sourceData.Lux),wakeTime(idx),bedTime(idx),2,3);
+    [out.firstCS{i1},out.lastCS{i1}] = firstLast(sourceData.Time,...
+        sourceData.CS,wakeTime(idx),bedTime(idx),firstHrs,lastHrs);
+    [out.firstCLA{i1},out.lastCLA{i1}] = firstLast(sourceData.Time,...
+        log(sourceData.CLA),wakeTime(idx),bedTime(idx),firstHrs,lastHrs);
+    [out.firstLux{i1},out.lastLux{i1}] = firstLast(sourceData.Time,...
+        log(sourceData.Lux),wakeTime(idx),bedTime(idx),firstHrs,lastHrs);
 end
 
 %% Save the output
-
+outputPath = fullfile(outputDir,['firstLastOutput_',...
+    datestr(now,'yy-mm-dd_HHMM'),'.mat']);
+save(outputPath,'out');
+% Convert to Excel
+% organizeExcel(outputPath);
 end
