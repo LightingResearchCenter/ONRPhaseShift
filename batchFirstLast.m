e17e4c172922cb64203d584fcb37bf2bb3f0f6bf
Geoffrey Jones <jonesg5@rpi.edu>
2013-10-17 11:03:38 -0400

    Generalized first/last

--- batchFirstLast.m
+++ batchFirstLast.m
@@ -1,30 +1,46 @@
-function firstLastCS
+function batchFirstLast
 addpath('phasorReport');
 
-%% Select input and output directories
+%% File handling
 startDir = fullfile([filesep,filesep],'root','projects',...
     'ONR PhaseShift');
 inputDir = fullfile(startDir,'dimesimeterData');
 outputDir = fullfile(startDir,'results');
 dirInfo = dir(fullfile(inputDir,'ref*.mat'));
+workbookFile = fullfile(inputDir,'index.xlsx');
+[subject,~,~,~,~,~,~,wakeTime,bedTime] = importIndex(workbookFile);
 
-%% Process files
+%% Assign constants
+firstHrs = 2; % Number of hours after waking
+lastHrs = 3; % Number of hours before bed
+n1 = length(dirInfo); % Number of files to be analyzed
 
-n1 = length(dirInfo);
+%% Preallocate output variables
+
+%% Analyze files
 for i1 = 1:n1
-    % Load file and decompose file name
+    % Load the file
     load(fullfile(inputDir,dirInfo(i1).name),'sourceData');
     fileName = regexprep(dirInfo(i1).name,'(.*)\.mat','$1');
+    % Decompose the file name
     fileParts = regexpi(fileName,...
         'ref(\d*)_(\d\d\d\d\d\d)-(\d\d\d\d\d\d)_sub(\d\d)_(\w*)_(\w*)',...
         'tokens');
-    fileTitle = {['Reference: ',fileParts{1}{1},' ',...
-        datestr(datenum(fileParts{1}{2},'yymmdd')),' - ',...
-        datestr(datenum(fileParts{1}{3},'yymmdd'))];...
-        ['Subject: ',fileParts{1}{4},' ',fileParts{1}{5},' ',fileParts{1}{6}]};
-%     PhasorReport(,,sourceData.Activity,fileTitle)
-    [firstCS,lastCS] = firstLast(sourceData.Time,sourceData.CS,wakeTime,bedTime,2,3)
+%     curReference{i1} = fileParts{1}{1};
+    curSubject{i1} = fileParts{1}{4};
+    curProtocol{i1} = fileParts{1}{5};
+    curStage{i1} = fileParts{1}{6};
+    % Find matching index entry
+    idx = strcmpi(curSubject{i1},subject);
+    %% Find first and last values
+    [firstCS{i1},lastCS{i1}] = firstLast(sourceData.Time,...
+        sourceData.CS,wakeTime(idx),bedTime(idx),2,3);
+    [firstCLA{i1},lastCLA{i1}] = firstLast(sourceData.Time,...
+        log(sourceData.CLA),wakeTime(idx),bedTime(idx),2,3);
+    [firstLux{i1},lastLux{i1}] = firstLast(sourceData.Time,...
+        log(sourceData.Lux),wakeTime(idx),bedTime(idx),2,3);
 end
 
+%% Save the output
 
 end
--- firstLast.m
+++ firstLast.m
@@ -1,11 +1,11 @@
-function [firstCS,lastCS] = firstLast(Time,CS,wakeTime,bedTime,firstHrs,lastHrs)
+function [first,last] = firstLast(Time,value,wakeTime,bedTime,firstHrs,lastHrs)
 %FIRSTLAST Summary of this function goes here
 %   Detailed explanation goes here
 
 % Create a time index with no date data
 TI = mod(Time,1);
 
-%% Calculate first CS after waking
+%% Calculate first value after waking
 % Find the end time that is the specified amount of hours after waking
 afterWake = mod(wakeTime+firstHrs/24,1);
 % Find the times that meet the requirements
@@ -15,9 +15,9 @@ else % Interval does NOT cross midnight
     firstIdx = TI >= wakeTime & TI <= afterWake;
 end
 % Average the values during the specified times
-firstCS = mean(CS(firstIdx));
+first = mean(value(firstIdx));
 
-%% Calculate last CS before bed
+%% Calculate last value before bed
 % Find the start time that is the specified amount of hours before bed
 beforeBed = mod(bedTime-lastHrs/24,1);
 % Find the times that meet the requirements
@@ -27,7 +27,7 @@ else % Interval does NOT cross midnight
     lastIdx = TI >= beforeBed & TI <= bedTime;
 end
 % Average the values during the specified times
-lastCS = mean(CS(lastIdx));
+last = mean(value(lastIdx));
 
 end
 
