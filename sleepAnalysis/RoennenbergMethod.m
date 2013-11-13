2252e276f6123f1c9591f329ca7e5172dd2fb0e1
Geoffrey Jones <jonesg5@rpi.edu>
2013-11-13 13:10:50 -0500

    Created Chronotype analysis

--- sleepAnalysis/RoennenbergMethod.m
+++ sleepAnalysis/RoennenbergMethod.m
@@ -1,42 +1,46 @@
-function [SO,SE,SD,WD] = RoennenbergMethod(Activity,Time,bedTime,wakeTime)
+function [SO,SE,SD,MS,W] = RoennenbergMethod(t,AI,SPrep,GU)
 %ROENNENBERGMETHOD Calculates sleep parameters using Roennenberg method
 %   Combines Actiware sleep algorithim with Roennenberg sleep parameters
+%   t = t in matlab DATENUM format
+%   AI = activity index
+%   SPrep = local time of preparing to sleep
+%   GU = local time of getting out of bed
 
 %% Trim data to within the analysis plus a buffer
 buffer = 5/(60*24); % 5 minute buffer
-idx = Time >= (bedTime - buffer) & Time <= (wakeTime + buffer);
-Time = Time(idx);
-if numel(Time) == 0
-    error('bed time and wake time out of bounds');
+idx = t >= (SPrep - buffer) & t <= (GU + buffer);
+t = t(idx);
+if numel(t) == 0
+    error('sleep prep and get up time out of range');
 end
-Activity = Activity(idx);
+AI = AI(idx);
 
-%% Determine if the analysis ends on a weekday
-dayNumber = weekday(Time(end));
+%% Determine if the analysis ends on a workday
+dayNumber = weekday(t(end));
 if dayNumber == 1 || dayNumber == 7
-    WD = false;
+    W = false;
 else
-    WD = true;
+    W = true;
 end
 
 %% Attempt to apply a gaussian filter to Activity
 try
-    Activity = gaussian(Activity,4);
+    AI = gaussian(AI,4);
 catch err
     warning(err.message);
 end
 
 %% Convert time to seconds from start of first day
-Time = (Time - floor(Time(1)))*24*60*60;
+tSec = (t - floor(t(1)))*86400;
 
 %% Calculate the sampling epoch to the nearest second
-tempTime1 = Time(2:end);
-tempTime2 = circshift(Time,1);
-tempTime2(1) = [];
-epoch = round(mean(abs(tempTime1-tempTime2)));
+tempSec1 = tSec(2:end);
+tempSec2 = circshift(tSec,1);
+tempSec2(1) = [];
+epoch = round(mean(abs(tempSec1-tempSec2)));
 
 %% Find the sleep state (SS)
-SS = FindSleepState(Activity,'auto',3);
+SS = FindSleepState(AI,'auto',3);
 
 %% Find sleep onset (SO) and sleep end (SE)
 % Find sleep state in a 10 minute window
@@ -48,24 +52,27 @@ for i1 = -n5:n5
     AS10 = AS10 + circshift(AS,i1);
 end
 SS10 = AS10 <= 1; % Create 10 minute sleep state (SS10)
-Time2 = Time;
+tSec2 = tSec;
 
 % Remove first and last 10 minutes
-last = numel(Time2);
-Time2((last-n5):last) = [];
+last = numel(tSec2);
+tSec2((last-n5):last) = [];
 SS10((last-n5):last) = [];
-Time2(1:n5) = [];
+tSec2(1:n5) = [];
 SS10(1:n5) = [];
 
 % Find sleep onset (SO)
 idxO = find(SS10,true,'first');
-SO = Time2(idxO);
+SO = tSec2(idxO);
 
 % Find sleep end (SE)
 idxE = find(SS10,true,'last');
-SE = Time2(idxE);
+SE = tSec2(idxE);
 
 %% Calculate sleep duration (SD) in seconds
 SD = abs(SE - SO);
+
+%% Calculate basic mid-sleep (MS) in seconds
+MS = SO + SD/2;
 
 end
